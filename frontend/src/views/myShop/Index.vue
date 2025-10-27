675d5b549a1936a55507d0836b9f468f3c559e8a
ljj1410446243 <1410446243@qq.com>
2025-10-27 21:57:59 +0800

    继续完善店铺功能-创建店铺

--- frontend/src/views/myShop/Index.vue
+++ frontend/src/views/myShop/Index.vue
@@ -17,7 +17,7 @@
             店铺名称
           </div>
         </template>
-        {{myShopInfo.name}}
+        {{myShopInfo.name||'暂无店铺'}}
       </el-descriptions-item>
       <el-descriptions-item>
         <template #label>
@@ -28,7 +28,7 @@
             店铺状态
           </div>
         </template>
-        <el-tag size="small">{{myShopInfo.status}}</el-tag>
+        <el-tag size="small">{{myShopInfo.status||'未创建'}}</el-tag>
 
       </el-descriptions-item>
       <el-descriptions-item>
@@ -40,7 +40,7 @@
             现金余额
           </div>
         </template>
-        {{ myShopInfo.cash_balance }}
+        {{ myShopInfo.cash_balance ? `￥${myShopInfo.cash_balance}`:'--' }}
       </el-descriptions-item>
       <el-descriptions-item>
         <template #label>
@@ -75,16 +75,23 @@
       label-position="left"
       label-width="100"
       :model="shopForm"
+      :rules="rules"
     >
-      <el-form-item label="店铺名称">
-        <el-input placeholder="请输入店铺名称">
+      <el-form-item label="店铺名称" prop="name">
+        <el-input
+            placeholder="请输入店铺名称"
+            v-model="shopForm.name"
+        >
         </el-input>
       </el-form-item>
-      <el-form-item label="店铺初始资金">
-        <el-input placeholder="请输入店铺初始资金">
+      <el-form-item label="初始资金" prop="cash_balance">
+        <el-input
+            placeholder="请输入店铺初始资金"
+            v-model="shopForm.cash_balance"
+        >
         </el-input>
       </el-form-item>
-      <el-button type="primary">创建</el-button>
+      <el-button type="primary" @click="handleCreateShop">创建</el-button>
     </el-form>
 
   </el-dialog>
@@ -92,18 +99,53 @@
 <script setup lang="ts">
 import {createShop, getMyShopInfo} from "../../api";
 import {onMounted, reactive, ref} from "vue";
+import {ElMessage} from "element-plus";
 
 const dialogVisible=ref(false)
 const myShopInfo=reactive({})
 
+const shopForm=reactive({
+  name:'',
+  cash_balance:5000//默认初始资金
+})
 
+const formRef=ref()
+const rules={
+  name:[
+    {required:true,message:'店铺名称不可为空',trigger:'blur'},
+    {min:2,max:10,message:'店铺名称长度为2-10个字符',trigger: 'blur'}
+  ],
+  cash_balance:[
+    {required:true,message:'初始资金不可为空',trigger:'blur'},
+  ]
+}
 
-onMounted(()=>{
+//获取店铺信息
+const fetchShopInfo=()=>{
   getMyShopInfo().then(data=>{
-    Object.assign(myShopInfo, data)
+    Object.assign(myShopInfo,data)
     console.log('获取店铺信息成功:', data)
-    console.log('注入店铺信息成功:', myShopInfo)
+  }).catch(err=>{
+    console.error('获取店铺信息失败:', err)
+    ElMessage.error('获取店铺信息失败')
   })
+}
+
+//创建店铺
+const handleCreateShop=()=>{
+  if(!formRef.value) return
+  formRef.value.validate().then(()=>{
+    const result=createShop(shopForm)
+    result.then((result)=>{
+      console.log('创建店铺成功：',result)
+      ElMessage.success('创建店铺成功!')
+      dialogVisible.value=false
+      fetchShopInfo()//刷新店铺信息
+    })
+  })
+}
+onMounted(()=>{
+  fetchShopInfo()
 })
 </script>
 
